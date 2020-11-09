cc6d419f6da42ce41a321307de46f2251649d920
zhaodafei <13681478010@163.com>
2020-11-09 18:05:44 +0800

    init

--- src/main/java/com/dafei/dao/UserDao.java
+++ src/main/java/com/dafei/dao/UserDao.java
@@ -1,13 +1,16 @@
 package com.dafei.dao;
 
 import com.dafei.model.User;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public interface UserDao {
 
     // 查询一条数据
-    User selectUser(long id);
+    // User selectUser(long id);
+    User selectUser(@Param("id") long id);
 
     /**
      *  和 mapper/UserMapper.xml 中的id="selectUserFoo"  对应
@@ -21,12 +24,15 @@ public interface UserDao {
     List<User> selectAllUser();
 
     // 添加
-    void save(User user);
+    // void save(User user);
+    void save(@RequestBody User user);
 
     // 修改
-    int update(User user);
+    // int update(User user);
+    int update(@RequestBody User user);
 
     // 删除
-    boolean delete(int id);
+    // boolean delete(int id);
+    boolean delete(@Param("id") int id);
 
 }
